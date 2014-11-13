2a77d8c6237b9111ccee9a7f8661eb66207fb80d
Chad Huneycutt <chadh@cc.gatech.edu>
2014-11-13 14:00:02 -0500

    adds aliases for puppetserver and puppetdb

--- hosts.pp
+++ hosts.pp
@@ -1,16 +1,18 @@
 host { 'localhost.localdomain':
-	ensure => present,
-	host_aliases => 'localhost',
-	ip => '127.0.0.1',
+  ensure => present,
+  host_aliases => 'localhost',
+  ip => '127.0.0.1',
 }
+
 host { 'trantor.vm':
-	ensure => present,
-	host_aliases => 'trantor',
-	ip => '10.250.0.10',
+  ensure => present,
+  host_aliases => [ 'trantor', 'trantor-puppetserver', 'trantor-puppetserver.vm', 'trantor-puppetdb', 'trantor-puppetdb.vm' ],
+  ip => '10.250.0.10',
 }
+
 host { 'anacreon.vm':
-	ensure => present,
-	host_aliases => 'anacreon',
-	ip => '10.250.0.11',
+  ensure => present,
+  host_aliases => 'anacreon',
+  ip => '10.250.0.11',
 }
 
