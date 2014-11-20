d4d52920d38c297a94a0dab81a26a4bd180e3a43
Chad Huneycutt <chad.huneycutt@gatech.edu>
2014-11-20 00:30:01 -0500

    updates vagrant bootstrap to pull rhel docker image from our server

--- trantor/bootstrap.sh
+++ trantor/bootstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set -x
 lvresize -L+5G /dev/vg1/var && resize2fs /dev/vg1/var
-puppet apply puppetserver/docker_shared/hosts.pp
+puppet apply /vagrant/docker_shared/hosts.pp
 yum install -y python-pip libyaml-devel python-devel
 pip install -U fig
-cat /vagrant/rhel6.docker.tgz | docker import - rhel:6.5
+# the order of the next two lines is important.  The docker-io-1.2.0-3.el6
+# version of docker has a bug that disallows tags on docker import.  So do the
+# import, and *then* update docker.
+curl -s http://linux-install.cc.gatech.edu/boxes/rhel6.docker.tgz | docker import - rhel:6.5
 yum install -y docker-io && service docker restart
+
